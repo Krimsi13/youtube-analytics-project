88a5e0512ad6b1c3b7d1827a5d12ceef4647015b
Krimsi13 <aleksejkrimsi@gmail.com>
2024-01-14 21:07:55 +0300

    hw-2

--- src/channel.py
+++ src/channel.py
@@ -11,9 +11,33 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def to_json(self, js):
+        with open(js, "w", encoding="utf-8") as f:
+            f.write(f"id канала: {self.channel_id}\n")
+            f.write(f"Название канала: {self.title}\n")
+            f.write(f"Описание канала: {self.description}\n")
+            f.write(f"Cсылка на канал: {self.url}\n")
+            f.write(f"Количество подписчиков: {self.subscriber_count}\n")
+            f.write(f"Количество видео: {self.video_count}\n")
+            f.write(f"Общее количество просмотров: {self.view_count}\n\n")
